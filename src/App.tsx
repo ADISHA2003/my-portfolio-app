5bb0092bbac51b4999a527e06b51036b694bf9a5
Aditya Sharma <adityasharma56697@gmail.com>
2024-11-11 12:51:17 +0530

    corrected the file

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Services from './components/Services';
-{/*import Plans from './components/Plans';*/}
 // import Plans from './components/Plans';
 import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
