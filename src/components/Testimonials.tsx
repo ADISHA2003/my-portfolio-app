c5e22f1929517f55b4f0aa768af81f8776d4f37c
Adisha2003 <adityasharma56697@gmail.com>
2025-05-18 01:44:04 +0530

    updated testimonials section

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -8,7 +8,7 @@ export default function Testimonials() {
         <div className="max-w-4xl mx-auto">
           <div className="bg-white p-8 rounded-lg shadow-md">
             <p className="text-gray-600 mb-4">
-              "We had the pleasure of having Aditya Sharma intern with us as a Front-End Development Intern from August 2024 to October 2024. During his time at Realezi, Aditya demonstrated dedication, growth, and a strong work ethic. His contributions to our projects were valuable, and his skills in HTML, CSS, JavaScript, and UI/UX design made a positive impact. We commend his commitment to the role and wish him all the best in his future endeavors."
+              "We had the pleasure of having Aditya Sharma intern with us as a Front-End Developer. During his time at Realezi, Aditya demonstrated dedication, growth, and a strong work ethic. His contributions to our projects were valuable, and his skills in HTML, CSS, JavaScript, and UI/UX design made a positive impact. We commend his commitment to the role and wish him all the best in his future endeavors."
             </p>
             <h4 className="font-bold">Hitesh Udhwani</h4>
             <p className="text-gray-600">Founder & CEO Realezi</p>
